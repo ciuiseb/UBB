5c5b24f359af49577d095c74e0905968c5a04df1
seb <ciuisebi@gmail.com>
2024-11-21 15:42:36 +0200

    a

--- Semestrul 3/MAP/lab 6-7/src/main/java/ubb/scs/map/domain/validators/Validator.java
+++
@@ -1,7 +0,0 @@
-package ubb.scs.map.domain.validators;
-
-import ubb.scs.map.domain.exceptions.ValidationException;
-
-public interface Validator<T> {
-    void validate(T entity) throws ValidationException;
-}
--- Semestrul 3/MAP/lab 6-7/src/main/java/ubb/scs/map/repository/FriendshipRepository.java
+++
@@ -1,9 +0,0 @@
-package ubb.scs.map.repository;
-
-import ubb.scs.map.domain.entities.Friendship;
-
-import java.util.Optional;
-
-public interface FriendshipRepository extends Repository<Long, Friendship>{
-    Optional<Friendship> deleteFriendship(Long userID1, Long userID2);
-}
