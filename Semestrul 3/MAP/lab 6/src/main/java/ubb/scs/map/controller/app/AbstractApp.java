4edfd79a9c12b250c797b57ad152aa32e4532f99
seb <ciuisebi@gmail.com>
2024-11-21 15:39:26 +0200

    a

--- Semestrul 3/MAP/lab 6/src/main/java/ubb/scs/map/controller/app/AbstractApp.java
+++
@@ -1,6 +0,0 @@
-package ubb.scs.map.controller.app;
-
-public interface AbstractApp {
-    public abstract void printMenu();
-    public abstract void run();
-}
--- Semestrul 3/MAP/lab 6/src/main/java/ubb/scs/map/domain/exceptions/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package ubb.scs.map.domain.exceptions;
-
-public class ServiceException extends RuntimeException {
-    public ServiceException(String message) {
-        super(message);
-    }
-}
